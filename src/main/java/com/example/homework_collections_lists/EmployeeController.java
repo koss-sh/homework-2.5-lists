b70d4a2fff7721da35e26f94d70f29e097467d09
koss-sh <shilinskii22@gmail.com>
2023-03-29 20:59:41 +0300

    ДЗ на проверку

--- src/main/java/com/example/homework_collections_lists/EmployeeController.java
+++ src/main/java/com/example/homework_collections_lists/EmployeeController.java
@@ -1,10 +1,40 @@
 package com.example.homework_collections_lists;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
+@RequestMapping("/employee")
 public class EmployeeController {
     private final EmployeeService employeeService;
+
     public EmployeeController(EmployeeService employeeService) {
         this.employeeService = employeeService;
     }
+    @GetMapping("/add")
+    public Employee add(@RequestParam(value = "firstName", required = false) String firstName,
+                                     @RequestParam(value = "lastName", required = false) String lastName) {
+        return employeeService.addEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/remove")
+    public Employee remove(@RequestParam(value = "firstName", required = false) String firstName,
+                        @RequestParam(value = "lastName", required = false) String lastName) {
+        return employeeService.removeEmployee(firstName, lastName);
+    }
+
+    @GetMapping("/find")
+    public Employee find(@RequestParam(value = "firstName", required = false) String firstName,
+                     @RequestParam(value = "lastName", required = false) String lastName) {
+        return employeeService.findEmployee(firstName, lastName);
+    }
+
+    @GetMapping
+    public List<Employee> getEmployees() {
+        return employeeService.employees;
+    }
+}
--- src/main/java/com/example/homework_collections_lists/EmployeeService.java
+++ src/main/java/com/example/homework_collections_lists/EmployeeService.java
@@ -1,29 +1,34 @@
 package com.example.homework_collections_lists;
 
+import org.springframework.stereotype.Service;
+
 import java.util.ArrayList;
 import java.util.List;
-
+@Service
 public class EmployeeService {
      List<Employee> employees = new ArrayList<>(List.of(
              new Employee("Иван", "Бадин"),
              new Employee("Вадим", "Питун"),
              new Employee("Анжела", "Меркелова")));
 
-          public void addEmployee(String firstName, String lastName) {
-          Employee newEmployee = new Employee(firstName, lastName);
-              if (employees.size() == 5) throw new EmployeeStorageIsFullException();
+          public Employee addEmployee(String firstName, String lastName) {
+              Employee newEmployee = new Employee(firstName, lastName);
+              if (employees.size() == 4) throw new EmployeeStorageIsFullException();
               for (Employee e: employees) {
                   if (e.getFirstName().equals(firstName) && e.getLastName().equals(lastName))
                       throw new EmployeeAlreadyAddedException();
               }
-          employees.add(newEmployee);
-
+              employees.add(newEmployee);
+              return newEmployee;
           }
-          public void removeEmployee (String firstName, String lastName) {
+          public Employee removeEmployee (String firstName, String lastName) {
               for (Employee e: employees) {
-                  if (e.getFirstName().equals(firstName) && e.getLastName().equals(lastName))
+                  if (e.getFirstName().equals(firstName) && e.getLastName().equals(lastName)) {
                       employees.remove(e);
+                      return e;
+                  }
               }
+              throw new EmployeeNotFoundException();
           }
          public Employee findEmployee (String firstName, String lastName) {
              for (Employee e : employees) {
@@ -32,7 +37,8 @@ public class EmployeeService {
              }
              throw new EmployeeNotFoundException();
          }
-    }
+
+}
 
 
 
